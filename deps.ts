2788643d23fd75095407da9c339653f070123be8
Henry Barreto <contato@henrybarreto.dev>
2021-02-20 14:32:12 -0300

    Improving the task to generate the compiled file
    Fixing drake problem which did not let the compiled file run

--- deps.ts
+++ deps.ts
@@ -8,5 +8,3 @@ export { DB } from "https://deno.land/x/sqlite/mod.ts";
 
 import Denomander from "https://deno.land/x/denomander@0.8.0/mod.ts";
 export default Denomander;
-
-export { desc, run, task, sh } from "https://deno.land/x/drake@v1.4.6/mod.ts";
--- tasks.ts
+++ tasks.ts
@@ -1,8 +1,9 @@
-import { desc, run, task, sh } from "./deps.ts";
+import { desc, run, task, sh } from "https://deno.land/x/drake@v1.4.6/mod.ts";
 
 desc("Creating the binary file");
 task("compile", [], async () => {
-  sh("deno compile --unstable ./src/merchant.ts --output ./bin/merchant");
+  sh("deno compile --unstable --lite --output ./bin/merchant ./src/merchant.ts");
+  sh("cp --force ./merchant.db ./bin");
 });
 
 desc("Creating database table and seeding it");
