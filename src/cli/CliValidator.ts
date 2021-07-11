90b9481effc76a3f7a47636ea399195ca45ca049
Henry Barreto <contato@henrybarreto.dev>
2021-07-10 21:18:53 -0300

    Leting the binary file and db files in the root project's root

--- src/cli/CliValidator.ts
+++ src/cli/CliValidator.ts
@@ -2,6 +2,9 @@ import { Order } from "../types.ts";
 
 export default class CliValidator {
   public static validateOrder(order: String): Order {
+    if(!order) {
+      return Order.Price;
+    }
     order = order.charAt(0).toUpperCase() + order.slice(1);
     const orderToCheck = order as keyof typeof Order;
     const hasValidOrder = Order[orderToCheck];
--- tasks.ts
+++ tasks.ts
@@ -3,14 +3,14 @@ import { desc, run, sh, task } from "https://deno.land/x/drake@v1.4.6/mod.ts";
 desc("Creating the binary file");
 task("compile", [], async () => {
   sh(
-    "deno compile --unstable --output ./bin/merchant --allow-all ./src/cli.ts",
+    "deno compile --unstable --output ./merchant --allow-all ./src/cli.ts",
   );
 });
 
 desc("Creating database table and seeding it");
 task("database", [], async () => {
   sh(
-    "deno run -A ./src/migrations/create_table_products.ts && deno run -A ./src/migrations/seed_table_products.ts && mv ./merchant.db ./bin",
+    "deno run -A ./src/migrations/create_table_products.ts && deno run -A ./src/migrations/seed_table_products.ts",
   );
 });
 
